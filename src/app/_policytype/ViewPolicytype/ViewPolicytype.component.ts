70a67fbd5270fa4a285add0e150f80daee12a07a
thifaya <TMollootimile@skhomotech.co.za>
2019-07-27 13:30:27 +0200

    small modifications.

--- src/app/_policytype/ViewPolicytype/ViewPolicytype.component.ts
+++ src/app/_policytype/ViewPolicytype/ViewPolicytype.component.ts
@@ -1,7 +1,7 @@
 
 
 import { Component, OnInit, OnChanges, AfterViewInit, SimpleChanges } from '@angular/core';
-import { FormControl, FormGroupDirective, NgForm, Validators, FormArray, FormGroup, FormBuilder,FormsModule } from '@angular/forms';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormArray, FormGroup, FormBuilder, FormsModule } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { Router } from '@angular/router';
 
@@ -27,60 +27,66 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
     styleUrls: ['./ViewPolicytype.component.css']
 })
 export class ViewPolicytypeComponent implements OnInit {
-    constructor( private formBuilder: FormBuilder,private _role: RoleService,private _policytype: PolicytypeService, private _router: Router, private app: AppComponent) { }
- 
-//////Initialized variables  
+    constructor(private formBuilder: FormBuilder, private _role: RoleService, private _policytype: PolicytypeService, private _router: Router, private app: AppComponent) { }
 
-    policytypes:Policytype[];
-    
-    
-   
-////Functions//////////////////////////////////////////
+    //////Initialized variables  
 
-ViewPolicytype(index){
-
-  localStorage.setItem('selectedPolicyType',JSON.stringify(this.policytypes[index]));
-
-    
-  swal({
-    title: 'Edit Policy Type '+this.policytypes[index].name+'',
-    text: "Are you sure you want to edit policy type? Please note if you make any changes to the premium it will affect the policy holder immediately ",
-    type: 'warning',
-    showCancelButton: true,
-    confirmButtonClass: 'btn btn-success',
-    cancelButtonClass: 'btn btn-danger',
-    cancelButtonText: 'Cancel',
-    confirmButtonText: 'Yes',
-    buttonsStyling: false 
-}).then((result) => { this._router.navigate(['/policytype/editpolicytype']) })
-
-}
-
-
-ngOnInit() {
+    policytypes: Policytype[];
 
 
 
-//get all policytypes
-    this._policytype.getPolicytypes()
-    .subscribe(res => {
-        this.policytypes=res;
-        console.log(this.policytypes)
-    }, err => {
-      console.log(err);
-    });
+    ////Functions//////////////////////////////////////////
 
-    this.app.loading = false
-    
+    ViewPolicytype(index) {
+
+        localStorage.setItem('selectedPolicyType', JSON.stringify(this.policytypes[index]));
 
 
+        swal({
+            title: 'Edit Policy Type ' + this.policytypes[index].name + '',
+            text: "Are you sure you want to edit policy type? Please note if you make any changes to the premium it will affect the policy holder immediately ",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonClass: 'btn btn-success',
+            cancelButtonClass: 'btn btn-danger',
+            cancelButtonText: 'Cancel',
+            confirmButtonText: 'Yes',
+            buttonsStyling: false
+        }).then((result) => {
+            if (result.value) {
+
+                this._router.navigate(['/policytype/editpolicytype'])
+
+            }
+        })
+
+    }
 
 
-  
-    
+    ngOnInit() {
 
- 
 
-}
+
+        //get all policytypes
+        this._policytype.getPolicytypes()
+            .subscribe(res => {
+                this.policytypes = res;
+                console.log(this.policytypes)
+            }, err => {
+                console.log(err);
+            });
+
+        this.app.loading = false
+
+
+
+
+
+
+
+
+
+
+    }
 
 }
