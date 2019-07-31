199aead01433688aa9d8678f54754184825d8841
thifaya <TMollootimile@skhomotech.co.za>
2019-07-31 15:27:11 +0200

    update member..

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 import {enableProdMode} from '@angular/core';
+//https://github.com/smart-fuenral-parlour/Portal.git
+
 
 enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
